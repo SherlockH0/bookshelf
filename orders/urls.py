c752995aa7d38c13bec70e237185ffe2bb64cca9
SherlockH0 <ddmlf7@gmail.com>
2024-01-25 16:42:11 +0200

    Finish cart and wishlist templates

--- orders/urls.py
+++ orders/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('cart/', views.cart, name='cart')
+    path('cart/', views.cart, name='cart'),
+    path('wishlist/', views.wishlist, name='wishlist')
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from shop.models import Book
 
 # Create your views here.
 
+
 def cart(request):
     return render(request, 'orders/cart.html')
+
+
+def wishlist(request):
+    return render(request, 'orders/wishlist.html', {'object_list': Book.objects.all()})
