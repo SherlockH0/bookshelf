7c5f1cbb34f3d17dd9eba8c89bbb5fd9af287bf6
SherlockH0 <ddmlf7@gmail.com>
2024-01-31 23:28:00 +0200

    .

--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import dotenv
 
-dotenv.load_dotenv()
+# dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
