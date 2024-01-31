66e6b02710919067c2cc04d8fa962c9b78f863b6
SherlockH0 <ddmlf7@gmail.com>
2024-01-31 22:43:22 +0200

    Switch to postgresql

--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dotenv
+
+dotenv.load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,9 +84,13 @@ WSGI_APPLICATION = 'bookshelf.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
     }
 }
 
--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,2 @@
 pip install -r requirements.txt
-python3.11.4 manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput
