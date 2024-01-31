6ae1b4cee1a911a8bffa676a37eb97ea17d8fc25
SherlockH0 <ddmlf7@gmail.com>
2024-01-31 23:47:24 +0200

    .

--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -130,8 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
