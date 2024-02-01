5f3a9a8a6af3ce8835c7f63b58fd409cdc52e659
SherlockH0 <ddmlf7@gmail.com>
2024-02-01 10:59:09 +0200

    Add debug value

--- bookshelf/urls.py
+++ bookshelf/urls.py
@@ -31,8 +31,6 @@ urlpatterns = [
     path('', include('orders.urls')),
 ]
 
-# urlpatterns += static(settings.STATIC_URL,
-# document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
