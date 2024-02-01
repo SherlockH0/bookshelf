9c61b211581c0b6e4b2e07daf2d382fd551e8adc
SherlockH0 <133522380+SherlockH0@users.noreply.github.com>
2024-02-01 17:52:22 +0200

    Update postgresql env variables names

--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'bookshelf.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "HOST": os.environ.get("DB_HOST"),
-        "PORT": os.environ.get("DB_PORT"),
+        "NAME": os.environ.get("POSTGRES_DATABASE"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": '',
     }
 }
 
