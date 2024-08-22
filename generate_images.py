68ff343df51fd70fdbf597ae42204d0134c133c5
SherlockH0 <ddmlf7@gmail.com>
2024-08-22 11:51:55 +0300

    Make migrations

--- generate_images.py
+++ generate_images.py
@@ -12,11 +12,7 @@ input_images = os.listdir(INPUT_DIR)
 
 
 for image_name in input_images:
-    out_name = (
-        lambda suffix: os.path.splitext(image_name)[0]
-        + suffix
-        + os.path.splitext(image_name)[1]
-    )
+    out_name = lambda suffix: os.path.splitext(image_name)[0] + suffix + ".webp"
     out_preview = os.path.join(OUTPUT_DIR, out_name("_preview"))
     out_cover = os.path.join(OUTPUT_DIR, out_name("_cover"))
     image_name = os.path.join(INPUT_DIR, image_name)
@@ -28,7 +24,7 @@ for image_name in input_images:
 
         preview_image.thumbnail(PREVIEW_SIZE, Image.Resampling.LANCZOS)
         cover_image.thumbnail(COVER_SIZE, Image.Resampling.LANCZOS)
-        preview_image.save(out_preview, "JPEG", quality=95)
-        cover_image.save(out_cover, "JPEG", quality=95)
+        preview_image.save(out_preview, "WEBP", quality=95)
+        cover_image.save(out_cover, "WEBP", quality=95)
     except IOError:
         print("cannot create thumbnail for '%s'" % image_name)
