f4d32bb7a38e95f256fc00f2d39264f08fb3d1b2
SherlockH0 <ddmlf7@gmail.com>
2024-08-30 12:21:37 +0300

    Remove debug toolbr url

--- bookshelf/urls.py
+++ bookshelf/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -31,7 +30,6 @@ urlpatterns = [
     path("logout/", user_views.CustomLogoutView.as_view(), name="logout"),
     path("", include("shop.urls")),
     path("", include("orders.urls")),
-] + debug_toolbar_urls()
-
+]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
