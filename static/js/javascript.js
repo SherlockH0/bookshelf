bede5ac6ee3e2c7f5afaac622ae13ddf65f4a36a
SherlockH0 <ddmlf7@gmail.com>
2024-09-02 17:15:45 +0300

    fix navbar

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -2,53 +2,43 @@
 
 const menuLinks = document.querySelector(".navbar__links");
 const menuToggle = document.querySelector(".navbar__menu-toggle");
-const menuBtn = document.querySelector('.menu-btn');
+const menuBtn = document.querySelector(".menu-btn");
 const highlight = document.querySelector(".mobile-highlight");
 const dropdowns = document.querySelectorAll(".dropdown");
 
 function close() {
   menuLinks.setAttribute("data-visible", false);
-  dropdowns.forEach((el) => {el.removeAttribute("open")});
-  highlight.setAttribute("closing", '');
-  menuBtn.classList.remove('open');
+  dropdowns.forEach((el) => {
+    el.removeAttribute("open");
+  });
+  highlight.setAttribute("closing", "");
+  menuBtn.classList.remove("open");
 
-  highlight.addEventListener("animationend", () => {
-    highlight.removeAttribute("closing");
-    highlight.setAttribute("data-visible", false);
-  }, {once: true});
+  highlight.addEventListener(
+    "animationend",
+    () => {
+      highlight.removeAttribute("closing");
+      highlight.setAttribute("data-visible", false);
+    },
+    { once: true },
+  );
 }
 
 function open() {
   menuLinks.setAttribute("data-visible", true);
   highlight.setAttribute("data-visible", true);
-  menuBtn.classList.add('open');
+  menuBtn.classList.add("open");
 }
 
 menuToggle.addEventListener("click", (e) => {
   const visibility = menuLinks.getAttribute("data-visible");
 
-  if (visibility === "false") open()
-  else if (visibility === "true") close()
-
+  if (visibility === "false") open();
+  else if (visibility === "true") close();
 });
 
 highlight.addEventListener("click", (e) => {
   const visibility = menuLinks.getAttribute("data-visible");
 
-  if (visibility === "true") close()
+  if (visibility === "true") close();
 });
-
-// Hide navbar on scroll
-
-const nav = document.querySelector('.navbar');
-let lastScrollY = window.scrollY;
-
-window.addEventListener('scroll', () => {
-  if (lastScrollY < window.scrollY && window.scrollY >= window.innerHeight * 0.7) {
-    nav.classList.add('nav--hidden')
-  } else {
-    nav.classList.remove('nav--hidden')
-  }
-  lastScrollY = window.scrollY
-});
-
