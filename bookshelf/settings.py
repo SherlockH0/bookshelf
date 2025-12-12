6aa38388d3b1b69718235641e12846df4b6fd788
 sherlock <apple@MacBook-Pro-apple.local>
2025-12-12 19:22:08 +0200

    verc

--- bookshelf/settings.py
+++ bookshelf/settings.py
@@ -116,7 +116,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
 
--- build_files.sh
+++
@@ -1,2 +0,0 @@
-python3.9 manage.py migrate
-python3.9 manage.py collectstatic --noinput
